e89b4215d9f0d7940a5ee743786a809d5182da04
Mirical Williams-Causton <44556130+Mirical-Williams-Causton@users.noreply.github.com>
2018-12-04 12:38:05 -0500

    Updated variables.swift
    Added information about declaring variables in swift

--- basic-syntax/variables.swift
+++ basic-syntax/variables.swift
@@ -1,1 +1,9 @@
-
+/**
+  Variables
+  - Declaring Variables 
+    - In swift, variables are declared using the 'var' keyword.
+    - Unlike most strongly-typed languages, you do not need a ';' after declaring your variables.
+    - Variables cannot be used before being initialized.
+    - Variable types must be implicited or explicited specified, meaning, variables either have to
+      be assigned a value to specify its type or explicitly specify its type upon declaration.
+*/
