14c2c19398949963638c2ad8d38024ef48a49860
Mirical Williams-Causton <44556130+Mirical-Williams-Causton@users.noreply.github.com>
2018-12-04 17:32:59 -0500

    Create operators.swift

---
+++ basic-syntax/operators.swift
@@ -0,0 +1,11 @@
+import Foundation
+
+/*
+ - Using Operators: Used to perform various functions such as value
+ assignments, mathematics, equality and logic comparisons, and declaring
+ ranges
+    - Arithmetic Operators: + - * /
+    - Remainder / Modulo Operators: %
+    - Compound Assignment Operators: += -= *= /= %=
+    - Comparison Operators: == <= >= < > !=
+    - Ternary Operators: ? : */
