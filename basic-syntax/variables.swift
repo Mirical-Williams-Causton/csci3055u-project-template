10d2a85cd1c426de384fb86dc63239a88e8be10c
Mirical Williams-Causton <44556130+Mirical-Williams-Causton@users.noreply.github.com>
2018-12-04 12:39:00 -0500

    Updated variables.swift

--- basic-syntax/variables.swift
+++ basic-syntax/variables.swift
@@ -5,5 +5,4 @@
     - Unlike most strongly-typed languages, you do not need a ';' after declaring your variables.
     - Variables cannot be used before being initialized.
     - Variable types must be implicited or explicited specified, meaning, variables either have to
-      be assigned a value to specify its type or explicitly specify its type upon declaration.
-*/
+      be assigned a value to specify its type or explicitly specify its type upon declaration. */
