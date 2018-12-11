30d3f2de28b2450714a8960deb769573e27805cb
Mirical-Williams-Causton <mirical.williamscauston@uoit.net>
2018-12-11 17:12:01 -0500

    Adding swift playground file for the standard library

---
+++ standard-library/StandardLibrary.playground/Contents.swift
@@ -0,0 +1,47 @@
+import Cocoa
+
+/* Some of the Swift Standard Library Functions */
+
+/* Arrays */
+
+    var numbers : Array <Int> = [4, 1, 3, 100, 5, 2, 56, 9]
+
+/* Dictionaries */
+
+    var students : Dictionary <String, Double> = ["Susan" : 30.9, "Alex" : 60.6, "Kent" : 95.3]
+
+/* Sort Function */
+
+    numbers.sort()
+
+/* Reverse Function */
+
+    numbers.reverse()
+
+/* Mathematical Functions */
+
+    /* Trigonometric Functions */
+
+        cos(20.0)
+        sin(20.0)
+        tan(20.0)
+
+    /* Logarithmic Function */
+
+        log(2.0)
+
+    /* Absolute Value Function */
+
+        abs(190.33445)
+
+    /* Square Root Function */
+
+        sqrt(9.0)
+
+    /* Random Function */
+
+        Int.random(in: 0 ..< 100)
+
+    /* Round Function */
+
+        round(89.9)
